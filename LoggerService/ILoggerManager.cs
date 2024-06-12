c6627fd0ed2077dc99273b355af87379cb4a12a5
Anish22prakash <anish2272000@gmail.com>
2024-06-12 18:53:14 +0530

    added the test comment

--- LoggerService/ILoggerManager.cs
+++ LoggerService/ILoggerManager.cs
@@ -6,5 +6,6 @@
         void LogError(string message, Exception exception = null);
         void LogDebug(string message);
         void LogWarning(string message);
+        // added the changes in logger service
     }
 }
